2dfbe73dd6c8a550f033bea725cfbded9a5e18db
Shumit Taher <Shumitt@gmail.com>
2025-02-16 21:57:23 +1300

    added a navbar

--- script.js
+++ script.js
@@ -13,4 +13,38 @@ document.addEventListener("DOMContentLoaded", function () {
     }, { threshold: 0.7 });
 
     observer.observe(midSection);
+
+    updateActiveStatus()
 });
+
+function updateActiveStatus() {
+    
+    let links = top_bar.querySelectorAll(".nav-link")
+    let activeRoute = location.pathname
+    
+    links.forEach((link)=>{
+        
+        if (link.pathname==activeRoute) {
+            link.classList.add("active")
+            link.ariaCurrent = "page"
+        } else {
+            link.classList.remove("active")
+            link.removeAttribute("ariaCurrent")
+        }
+
+    })
+
+    document.addEventListener('scroll', ()=>{
+            
+        if (window.scrollY === 0) {
+            top_bar.classList.add("at_top", activeRoute=='/'?"navbar-dark":"_");
+            top_bar.classList.remove("not_at_top"); 
+        } else {
+            top_bar.classList.add("not_at_top");
+            top_bar.classList.remove("at_top", activeRoute=='/'?"navbar-dark":"_");
+        }
+            
+    })
+
+
+}
